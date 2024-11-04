5d7a55f203524a5bf6b925c40fcf7423095e5d43
leh <leticiazanellattodeoliveira@gmail.com>
2024-11-04 14:26:56 -0300

    colocando o limite

--- backend/src/index.js
+++ backend/src/index.js
@@ -15,7 +15,7 @@ mongoose.connect(dbUri, {
 }).catch((err) => console.log(err))
 
 app.use(cors())
-app.use(express.json())
+app.use(express.json({ limit: '10mb' }))
 app.use(router)
 
 app.listen(3333, () => console.log('Server is running on port 3333'))
