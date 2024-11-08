8748f6a8ce23c9264a5ef59940e70f1b4614a246
leh <leticiazanellattodeoliveira@gmail.com>
2024-11-08 16:04:48 -0300

    original

--- backend/src/Validations/emailValidation.js
+++ backend/src/Validations/emailValidation.js
@@ -1,30 +1,24 @@
-const axios = require('axios');
+const axios = require('axios')
+require('dotenv').config()
 
-// Função para verificar o formato do email
-function isValidEmailFormat(email) {
-    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return regex.test(email);
+function isValidEmailFormat (email){
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+    return regex.test(email)
 }
 
-// Função serverless para verificar o email
-module.exports = async (req, res) => {
-    const { email } = req.body // Supõe que o email vem no corpo da requisição
+async function verifyEmail (email) {
+    if(!isValidEmailFormat(email)) return false    
 
-    if (!isValidEmailFormat(email)) {
-        return res.status(400).json({ message: 'Formato de email inválido' });
-    }
+    const url = `${process.env.URL_EMAIL}?email=${email}&api_key=${process.env.EMAIL_KEY}`;
 
-    const url = `${process.env.URL_EMAIL}?email=${email}&api_key=${process.env.EMAIL_KEY}`
-
-    try {
+    try{
         const response = await axios.get(url)
-        const isValid = response.data.data.status === 'valid'
-        return res.status(200).json({ valid: isValid })
-    } catch (err) {
-        console.error('Erro ao verificar o email:', err.message)
-        return res.status(500).json({ message: 'Erro ao verificar email' })
+        return response.data.data.status === 'valid'
+    }
+    catch(err){
+        //console.log(err)
+        return false
     }
 }
-
 
 module.exports = { isValidEmailFormat, verifyEmail }
