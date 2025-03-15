ff120cdef7d03103886e065c0bd9e22ab6113b75
leh <leticiazanellattodeoliveira@gmail.com>
2025-03-15 13:14:06 -0300

    agendar consulta e avaliar prioridade

--- backend/src/Controller/PsicologoController.js
+++ backend/src/Controller/PsicologoController.js
@@ -1,16 +1,17 @@
 const Ficha = require('../Models/FichaModel');
-const Psicologo = require('../Models/PsicologoModel');
 const User = require('../Models/UserModel');
 
 module.exports = {
 
     // Avaliar ficha e definir prioridade
-    async avaliarFicha(req, res) {
+    async prioridadeFicha(req, res) {
        
         const { ficha_id } = req.params;
         const { prioridade } = req.body; // 'Baixa', 'Média', 'Alta'
-        //const psicologoId = req.params; // Assumindo que `auth` contém o ID do psicólogo autenticado
         const { role } = req.headers;
+
+        const validRole = 'psicologo'
+        if(role != validRole) return res.status(400).send({ message: 'Somente psicologo pode avaliar' });
 
         if (!['Baixa', 'Média', 'Alta'].includes(prioridade)) {
             return res.status(400).send({ message: 'Prioridade inválida' });
@@ -20,20 +21,56 @@ module.exports = {
             const ficha = await Ficha.findById(ficha_id);
 
             if (!ficha) {
-                return res.status(404).send({ message: 'Ficha não encontrada' });
+                return res.status(400).send({ message: 'Ficha não encontrada' });
             }
 
             ficha.prioridade = prioridade;
-            //ficha.psicologoId = psicologoId;
-            //ficha.status = 'Avaliada';
+            ficha.status = 'Avaliada';
             ficha.triagem = false
 
             await ficha.save();
 
-            return res.status(200).send({ message: 'Ficha avaliada com sucesso', ficha });
+            return res.status(200).send({ 
+                message: 'Ficha avaliada com sucesso', 
+                ficha 
+            });
+
         } catch (err) {
-            return res.status(500).send({ message: 'Erro ao avaliar ficha', error: err.message });
+            return res.status(500).send({ 
+                message: 'Erro ao avaliar ficha', 
+                error: err.message 
+            });
         }
     },
 
+    async agendarTriagem(req, res){
+        const {ficha_id } = req.params;
+        const { agenda } = req.body;
+        const { role } = req.headers;
+
+        const validRole = 'psicologo'
+        if(role != validRole) return res.status(400).send({ message: 'Somente psicologo pode avaliar' });
+
+        try{
+            const ficha = await Ficha.findById(ficha_id);
+            if(!ficha) return res.status(400).send({ message: 'Ficha nao encontrada' });
+            
+            ficha.status = "Em avaliação";
+            ficha.agenda = agenda;
+
+            await ficha.save();
+
+            return res.status(200).send({
+                message: 'Triagem agendada com sucesso',
+                ficha
+            })
+        }
+        catch(err){
+            return res.status(400).send({
+                message: 'Erro ao agerndar paciente.',
+                error: err.message
+            });
+        }
+
+    }
 };
