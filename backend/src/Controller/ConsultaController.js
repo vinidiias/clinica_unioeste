2d4a7c66fe410cffe5952cea6545bddd01bf16ef
leh <leticiazanellattodeoliveira@gmail.com>
2025-03-15 15:51:18 -0300

    arrumado ivated e ficha model

--- backend/src/Controller/ConsultaController.js
+++ backend/src/Controller/ConsultaController.js
@@ -1,17 +1,23 @@
 const Ficha = require('../Models/FichaModel');
 const User = require('../Models/UserModel');
+const Consulta = require('../Models/ConsultaModel')
 
 module.exports = {
 
     // Avaliar ficha e definir prioridade
     async prioridadeFicha(req, res) {
        
-        const { ficha_id } = req.params;
+        const { ficha_id, user_id } = req.params;
         const { prioridade } = req.body; // 'Baixa', 'Média', 'Alta'
-        const { role } = req.headers;
+        const { auth } = req.headers;
+
+        const psico = await User.findById(user_id);
+        if(!psico) return res.status(400).send({ message: 'Usuario nao encontrado' });
+
+        if(user_id !== auth) return res.status(400).send({ message: 'Nao autorizado'});
 
         const validRole = 'psicologo'
-        if(role != validRole) return res.status(400).send({ message: 'Somente psicologo pode avaliar' });
+        if(psico.role != validRole) return res.status(400).send({ message: 'Somente psicologo pode avaliar' });
 
         if (!['Baixa', 'Média', 'Alta'].includes(prioridade)) {
             return res.status(400).send({ message: 'Prioridade inválida' });
@@ -44,12 +50,23 @@ module.exports = {
     },
 
     async agendarTriagem(req, res){
-        const {ficha_id } = req.params;
+        const {ficha_id, psico_id, paciente_id } = req.params;
         const { agenda } = req.body;
-        const { role } = req.headers;
+        const { auth } = req.headers;
 
-        const validRole = 'psicologo'
-        if(role != validRole) return res.status(400).send({ message: 'Somente psicologo pode avaliar' });
+        const psico = await User.findById(psico_id);
+        if(!psico) return res.status(400).send({ message: 'Psicologo nao encontrado' });
+
+        const paciente = await User.findById(paciente_id);
+        if(!paciente) return res.status(400).send({ message: 'Paciente nao encontrado'});
+
+        const validRolePaciente = 'paciente';
+        if(paciente.role !== validRolePaciente) return res
+
+        if(psico_id !== auth) return res.status(400).send({ message: 'Nao autorizado'});
+
+        const validRolePsico = 'psicologo'
+        if(psico.role !== validRolePsico) return res.status(400).send({ message: 'Somente psicologo pode avaliar' });
 
         try{
             const ficha = await Ficha.findById(ficha_id);
@@ -71,6 +88,5 @@ module.exports = {
                 error: err.message
             });
         }
-
     }
 };
